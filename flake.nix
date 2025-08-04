3b120255bf210daacebe329e7bef357ad2fb3f9e
Nabil Aldihni Garcia <aldihninabil@gmail.com>
2025-08-04 16:16:05 -0400

    Added home-manager

--- flake.nix
+++ flake.nix
@@ -3,19 +3,31 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }: 
+  outputs = { self, nixpkgs, home-manager, ... }: 
   let
     system = "x86_64-linux";
     lib = nixpkgs.lib;
+    pkgs = nixpkgs.legacyPackages.${system};
   in
   {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
-        system = system;
+        inherit system;
         modules = [ ./configuration.nix ];
       };
     };
+    homeConfigurations = {
+      nabil = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
   };
+
 }
