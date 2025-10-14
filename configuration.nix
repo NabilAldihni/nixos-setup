58bcac4ca92e92c2a216a021cf21cbe220af9b50
Nabil Aldihni Garcia <aldihninabil@gmail.com>
2025-10-14 11:38:59 -0400

    Removed osprober

--- configuration.nix
+++ configuration.nix
@@ -10,7 +10,7 @@
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.useOSProber = false;
 
   networking.hostName = "nixos";
 
