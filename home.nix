a7922b1c6e9c5bfb64e0a1f7a59c418c68e97e62
Nabil Aldihni Garcia <aldihninabil@gmail.com>
2026-01-06 15:55:13 +0100

    Added ssh configs

--- home.nix
+++ home.nix
@@ -45,6 +45,91 @@
     };
 
     programs.ssh = {
+        enable = true;
+        enableDefaultConfig = false;
+        matchBlocks = {
+            pwn-college = {
+                host = "pwn-college";
+                hostname = "dojo.pwn.college";
+                user = "hacker";
+                identityFile = "~/.ssh/id_ed25519";
+            };
 
+            debian-nginx = {
+                host = "debian-nginx";
+                hostname = "192.168.30.51";
+                user = "root";
+                identityFile = "~/.ssh/id_lab";
+            };
+
+            ubuntu-docker = {
+                host = "ubuntu-docker";
+                hostname = "192.168.10.51";
+                user = "nabil";
+                identityFile = "~/.ssh/id_lab";
+            };
+
+            debian-vault = {
+                host = "debian-vault";
+                hostname = "192.168.40.51";
+                user = "nabil";
+                identityFile = "~/.ssh/id_lab";
+            };
+
+            debian-mc = {
+                host = "debian-mc";
+                hostname = "192.168.10.52";
+                user = "nabil";
+                identityFile = "~/.ssh/id_lab";
+            };
+
+            k8s-ctrlr = {
+                host = "k8s-ctrlr";
+                hostname = "192.168.40.56";
+                user = "nabil";
+                identityFile = "~/.ssh/id_lab";
+            };
+
+            k8s-worker1 = {
+                host = "k8s-worker1";
+                hostname = "192.168.40.57";
+                user = "nabil";
+                identityFile = "~/.ssh/id_lab";
+            };
+
+            k8s-worker2 = {
+                host = "k8s-worker2";
+                hostname = "192.168.40.58";
+                user = "nabil";
+                identityFile = "~/.ssh/id_lab";
+            };
+
+            t2l-dev1 = {
+                host = "t2l-dev1";
+                hostname = "192.168.50.51";
+                user = "nabil";
+                identityFile = "~/.ssh/id_lab";
+            };
+
+            proxmox = {
+                host = "proxmox";
+                hostname = "100.118.68.78";
+                user = "root";
+                identityFile = "~/.ssh/id_lab";
+            };
+
+            "*" = {
+                forwardAgent = false;
+                addKeysToAgent = "no";
+                compression = false;
+                serverAliveInterval = 0;
+                serverAliveCountMax = 3;
+                hashKnownHosts = false;
+                userKnownHostsFile = "~/.ssh/known_hosts";
+                controlMaster = "no";
+                controlPath = "~/.ssh/master-%r@%n:%p";
+                controlPersist = "no";
+            };
+        };
     };
 }
