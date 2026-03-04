c33614c4c6d35e068ac6129a7cba57cd2e4205c8
Nabil Aldihni Garcia <aldihninabil@gmail.com>
2026-03-03 23:22:10 -0500

    added clang tools

--- configuration.nix
+++ configuration.nix
@@ -213,7 +213,7 @@ XC1gOVNVlwbBusLvURRFKs5S12ZvGjkGsfH4/izrl6+9ft87cUczbhY=
         bash
         git
         wget
-        gcc
+        gcc clang-tools
         alacritty
         kdePackages.dolphin
         mesa
