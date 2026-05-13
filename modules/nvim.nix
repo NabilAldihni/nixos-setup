cfdcd168849339b30c186f084045e03bb765add3
Nabil Aldihni Garcia <aldihninabil@gmail.com>
2026-05-13 14:05:58 -0400

    fixed nvim error

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -1,16 +1,25 @@
 { config, pkgs, ... }:
-{
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    vimAlias = true;
-
+let
+  nvim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
     withPython3 = false;
     withRuby = false;
-
-    extraPackages = with pkgs; [ gcc ];
+    vimAlias = true;
+    wrapRc = false;
+    extraMakeWrapperArgs = [
+      "--suffix"
+      "PATH"
+      ":"
+      (pkgs.lib.makeBinPath [ pkgs.gcc ])
+    ];
   };
+in
+{
+  home.packages = [ nvim ];
 
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+  };
 
   home.file.".config/nvim".source =
     config.lib.file.mkOutOfStoreSymlink
